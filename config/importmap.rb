47ee2d9b2477341f4805c5b4900ed62d02e22ced
jsangalang2 <jsangalang2@academic.rrc.ca>
2025-12-02 19:10:04 -0600

    removing all paypal related lines

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,5 +6,3 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-# config/importmap.rb
-pin "paypal", to: "https://www.paypal.com/sdk/js?client-id=AbdskrA24ugeb6yHnUw4JpaamGHjobDxwjKL2TMWAfJGScco13CsGNxZm0FrVyXfihV--6Rm3q5_OQiM&currency=USD&components=buttons"
